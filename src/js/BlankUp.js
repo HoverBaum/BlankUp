226e7ec248a5c1f6d35ef531b3f00df9d3210cba
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-06-19 15:47:21 +0800

    add basic preview

--- src/js/BlankUp.js
+++ src/js/BlankUp.js
@@ -1,72 +1,69 @@
-/*
-var URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
-navigator.saveBlob = navigator.saveBlob || navigator.msSaveBlob || navigator.mozSaveBlob || navigator.webkitSaveBlob;
-window.saveAs = window.saveAs || window.webkitSaveAs || window.mozSaveAs || window.msSaveAs;
-
 // Because highlight.js is a bit awkward at times
 var languageOverrides = {
-  js: 'javascript',
-  html: 'xml'
+    js: 'javascript',
+    html: 'xml'
 };
 
-emojify.setConfig({ img_dir: 'emoji' });
-
-var md = markdownit({
-  html: true,
-  highlight: function(code, lang){
-	if(languageOverrides[lang]) lang = languageOverrides[lang];
-	if(lang && hljs.getLanguage(lang)){
-	  try {
-		return hljs.highlight(lang, code).value;
-	  }catch(e){}
-	}
-	return '';
-  }
-})
-  .use(markdownitFootnote);
-
-
-function update(e){
-  setOutput(e.getValue());
-}
-
-function setOutput(val){
-  val = val.replace(/<equation>((.*?\n)*?.*?)<\/equation>/ig, function(a, b){
-	return '<img src="http://latex.codecogs.com/png.latex?' + encodeURIComponent(b) + '" />';
-  });
-
-  var out = document.getElementById('out');
-  var old = out.cloneNode(true);
-  out.innerHTML = md.render(val);
-  emojify.run(out);
-
-  var allold = old.getElementsByTagName("*");
-  if (allold === undefined) return;
-
-  var allnew = out.getElementsByTagName("*");
-  if (allnew === undefined) return;
-
-  for (var i = 0, max = Math.min(allold.length, allnew.length); i < max; i++) {
-	if (!allold[i].isEqualNode(allnew[i])) {
-	  out.scrollTop = allnew[i].offsetTop;
-	  return;
-	}
-  }
-}
-*/
-
-var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-  mode: 'gfm',
-  lineNumbers: false,
-  matchBrackets: true,
-  lineWrapping: true,
-  theme: 'genesis',
-  extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
+emojify.setConfig({
+    img_dir: 'emoji'
 });
 
-/*
-editor.on('change', update);
+//Get an instance of markdown-it to parse the markdown to HTML
+var md = markdownit({
+        html: true,
+        highlight: function(code, lang) {
+            if (languageOverrides[lang]) lang = languageOverrides[lang];
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return hljs.highlight(lang, code).value;
+                } catch (e) {}
+            }
+            return '';
+        }
+    })
+    .use(markdownitFootnote);
 
+
+function update(e) {
+    setOutput(e.getValue());
+}
+
+function setOutput(val) {
+    var out = document.querySelector('.BlankUp-preview');
+    var old = out.cloneNode(true);
+    out.innerHTML = md.render(val);
+    emojify.run(out);
+
+	//Scroll to the first node that changed.
+    var allold = old.getElementsByTagName("*");
+    if (allold === undefined) return;
+
+    var allnew = out.getElementsByTagName("*");
+    if (allnew === undefined) return;
+
+    for (var i = 0, max = Math.min(allold.length, allnew.length); i < max; i++) {
+        if (!allold[i].isEqualNode(allnew[i])) {
+            out.scrollTop = allnew[i].offsetTop;
+            return;
+        }
+    }
+}
+
+
+var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+    mode: 'gfm',
+    lineNumbers: false,
+    matchBrackets: true,
+    lineWrapping: true,
+    theme: 'genesis',
+    extraKeys: {
+        "Enter": "newlineAndIndentContinueMarkdownList"
+    }
+});
+
+
+editor.on('change', update);
+/*
 
 document.addEventListener('drop', function(e){
   e.preventDefault();
@@ -80,8 +77,6 @@ document.addEventListener('drop', function(e){
   reader.readAsText(e.dataTransfer.files[0]);
 }, false);
 
-
+*/
 
 update(editor)
-
-*/
